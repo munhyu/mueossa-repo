e27f5360c53128adff749dff57047acff054c824
MYH <ansdudgb@gmail.com>
2025-04-11 17:46:00 +0900

    암호화 문제 해결

--- src/main/java/com/smhrd/mueossa/controller/UserController.java
+++ src/main/java/com/smhrd/mueossa/controller/UserController.java
@@ -11,6 +11,8 @@ import com.smhrd.mueossa.Repository.UserRepository;
 import com.smhrd.mueossa.entity.TbUser;
 import com.smhrd.mueossa.model.User;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class UserController {
 
@@ -22,20 +24,25 @@ public class UserController {
   public String userInsert(User user, Model model) {
     TbUser tbUser = new TbUser(user);
     tbUser.setType("U");
+    // SHA256 암호화
+    tbUser.setPw(org.apache.commons.codec.digest.DigestUtils.sha256Hex(user.getPw()));
     userRepository.save(tbUser);
     return "redirect:/goPreference"; // 회원가입 후 로그인 페이지로 리다이렉트
   }
 
   // 로그인 처리
   @PostMapping("/userLogin")
-  public String userLogin(User user, Model model) {
+  public String userLogin(User user, HttpSession session, Model model) {
 
     // 비밀번호
-
     System.out.println("로그인 시도: ID=" + user.getId() + ", PW=" + user.getPw());
-    TbUser tbUser = userRepository.findByIdAndPw(user.getId(), user.getPw());
+    // SHA256 암호화
+    String pw = org.apache.commons.codec.digest.DigestUtils.sha256Hex(user.getPw());
+
+    TbUser tbUser = userRepository.findByIdAndPw(user.getId(), pw);
     if (tbUser != null) {
       System.out.println("로그인 성공");
+      session.setAttribute("user", tbUser); // 세션에 사용자 정보 저장
       return "redirect:/goHome"; // 메인 페이지로 리다이렉트
     } else {
       System.out.println("로그인 실패");
@@ -44,15 +51,11 @@ public class UserController {
     }
   }
 
-  // // 아이디 중복 체크
-  // @PostMapping("/checkId")
-  // @ResponseBody
-  // public boolean checkId(@RequestParam("id") String id) {
-  // TbUser tbUser = userRepository.findByUserId(id);
-  // if (tbUser != null) {
-  // return false; // 중복된 아이디가 존재하는 경우
-  // } else {
-  // return true; // 중복되지 않은 아이디인 경우
-  // }
-  // }
+  // 아이디 중복 체크
+  @PostMapping("/checkId")
+  @ResponseBody
+  public boolean checkId(@RequestParam("id") String id) {
+    // Optional 처리
+    return userRepository.findById(id).isEmpty(); // 중복되지 않은 경우 true 반환
+  }
 }

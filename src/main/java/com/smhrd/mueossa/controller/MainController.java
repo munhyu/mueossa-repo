d668b3f71c2411cd6204e203ef859a8fbb112e0f
taeyeong130 <qkrxodud13054@gmail.com>
2025-04-14 14:15:38 +0900

    0414pull

--- src/main/java/com/smhrd/mueossa/controller/MainController.java
+++ src/main/java/com/smhrd/mueossa/controller/MainController.java
@@ -1,10 +1,26 @@
 package com.smhrd.mueossa.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.smhrd.mueossa.Repository.SurveyRepository;
+import com.smhrd.mueossa.Repository.UserRepository;
+import com.smhrd.mueossa.entity.TbSurvey;
+import com.smhrd.mueossa.entity.TbUser;
+import com.smhrd.mueossa.model.Survey;
+import com.smhrd.mueossa.model.User;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
+
+  @Autowired
+  private SurveyRepository surveyRepository;
 
   // 기본 메인페이지
   @GetMapping("/")
@@ -27,7 +43,7 @@ public class MainController {
   // 로그인 페이지 이동
   @GetMapping("/goLogin")
   public String goLogin() {
-    return "login"; // main.html로 이동
+    return "login";
   }
 
   // 카테고리 페이지 이동
@@ -38,8 +54,13 @@ public class MainController {
 
   // 찜목록 페이지 이동
   @GetMapping("/goWishlist")
-  public String goWishlist() {
-    return "wishlist";
+  public String goWishlist(HttpSession session) {
+    TbUser loginUser = (TbUser) session.getAttribute("user");
+    if (loginUser == null) {
+      return "redirect:/goLogin";
+    } else {
+      return "wishlist";
+    }
   }
 
   // 마이페이지 페이지 이동
@@ -49,15 +70,45 @@ public class MainController {
   }
 
   // 취향선택 페이지 이동
+  // @GetMapping("/goPreference")
+  // public String goPreference(HttpSession session, Model model, Survey survey) {
+  // TbUser loginUser = (TbUser) session.getAttribute("user");
+  // if (loginUser == null) {
+  // String userId = loginUser.getId();
+  // surveyRepository.findById(userId);
+  // return "redirect:/goLogin";
+  // } else {
+  // return "preference";
+  // }
+  // }
   @GetMapping("/goPreference")
-  public String goPreference() {
+  public String goPreference(HttpSession session, Model model) {
+    TbUser loginUser = (TbUser) session.getAttribute("user");
+    // 로그인하지 않은 경우 로그인 페이지로 리다이렉트
+    if (loginUser == null) {
+      return "redirect:/goLogin";
+    }
+    String userId = loginUser.getId();
+    // 사용자 ID로 Survey 데이터 조회
+    Optional<TbSurvey> surveyOptional = surveyRepository.findById(userId);
+    // Survey 데이터가 존재하면 모델에 추가
+    if (surveyOptional.isPresent()) {
+      model.addAttribute("survey", surveyOptional.get());
+    } else {
+      model.addAttribute("survey", new TbSurvey()); // 빈 Survey 객체 추가
+    }
     return "preference";
   }
 
   // 회원 정보 수정 페이지 이동
   @GetMapping("/goUpdate")
-  public String goUpdate() {
-    return "update";
+  public String goUpdate(HttpSession session) {
+    TbUser loginUser = (TbUser) session.getAttribute("user");
+    if (loginUser == null) {
+      return "redirect:/goLogin";
+    } else {
+      return "update";
+    }
   }
 
 }

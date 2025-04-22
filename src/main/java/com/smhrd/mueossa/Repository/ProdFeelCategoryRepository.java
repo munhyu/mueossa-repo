423e4e8661a6e952f2afe89a24ec7a67ab878541
MYH <ansdudgb@gmail.com>
2025-04-22 16:14:16 +0900

    카테고리 선택 비동기화

--- src/main/java/com/smhrd/mueossa/Repository/ProdFeelCategoryRepository.java
+++ src/main/java/com/smhrd/mueossa/Repository/ProdFeelCategoryRepository.java
@@ -154,58 +154,6 @@ public interface ProdFeelCategoryRepository extends JpaRepository<TbProdFeelCate
 			nativeQuery = true)
 	Optional<ProdFeelCategoryPercentileDTO> findCategoryPercentiles(@Param("pd_id") String pd_id);
 
-	// 제품 필터링, 제품 카테고리 Y/N이 매개변수로 받은 값과 일치하는 제품을 조회하는 쿼리
-	@Query("SELECT NEW com.smhrd.mueossa.dto.ProductAndCategoryDTO(" +
-			"   p.pId, p.pBrand, p.pName, p.pLikes, p.pRating, p.pDiscount, p.pPrice, p.pGender, p.pLink, p.pImage, p.pGroup, p.pType, p.sentiment, "
-			+
-			"   CASE WHEN pfc.ctComf >= 26.47 THEN 'Y' ELSE 'N' END, " +
-			"   CASE WHEN pfc.ctFluffy >= 1.28 THEN 'Y' ELSE 'N' END, " +
-			"   CASE WHEN pfc.ctLight >= 2.4 THEN 'Y' ELSE 'N' END, " +
-			"   CASE WHEN pfc.ctSoft >= 0.48 THEN 'Y' ELSE 'N' END, " +
-			"   CASE WHEN pfc.ctFlat >= 1.74 THEN 'Y' ELSE 'N' END, " +
-			"   CASE WHEN pfc.ctPretty >= 34.96 THEN 'Y' ELSE 'N' END, " +
-			"   CASE WHEN pfc.ctCute >= 1.14 THEN 'Y' ELSE 'N' END, " +
-			"   CASE WHEN pfc.ctNeat >= 2.19 THEN 'Y' ELSE 'N' END, " +
-			"   CASE WHEN pfc.ctModern >= 0.52 THEN 'Y' ELSE 'N' END, " +
-			"   CASE WHEN pfc.ctHip >= 17.95 THEN 'Y' ELSE 'N' END, " + // 휘뚜루마뚜루
-			"   CASE WHEN pfc.ctWide >= 20.7 THEN 'Y' ELSE 'N' END, " + // 발볼이 넓은
-			"   CASE WHEN pfc.ctNarrow >= 15.47 THEN 'Y' ELSE 'N' END, " + // 발볼이 좁은
-			"   CASE WHEN pfc.ctStandard >= 23.66 THEN 'Y' ELSE 'N' END, " + // 발볼이 보통
-			"   CASE WHEN pfc.ctCost >= 10.1 THEN 'Y' ELSE 'N' END, " + // 가격이 착한
-			"   CASE WHEN pfc.ctStrong >= 3.12 THEN 'Y' ELSE 'N' END) " + // 튼튼한
-			"FROM TbProduct p JOIN TbProdFeelCategory pfc ON p.pId = pfc.pdId " +
-			"WHERE (:#{#filterForm.pGroup} IS NULL OR :#{#filterForm.pGroup} = '' OR p.pGroup = :#{#filterForm.pGroup}) " +
-			"  AND (:#{#filterForm.ctComf} IS NULL OR :#{#filterForm.ctComf} != 'Y' OR (CASE WHEN pfc.ctComf >= 26.47 THEN 'Y' ELSE 'N' END) = 'Y') "
-			+
-			"  AND (:#{#filterForm.ctFluffy} IS NULL OR :#{#filterForm.ctFluffy} != 'Y' OR (CASE WHEN pfc.ctFluffy >= 1.28 THEN 'Y' ELSE 'N' END) = 'Y') "
-			+
-			"  AND (:#{#filterForm.ctLight} IS NULL OR :#{#filterForm.ctLight} != 'Y' OR (CASE WHEN pfc.ctLight >= 2.4 THEN 'Y' ELSE 'N' END) = 'Y') "
-			+
-			"  AND (:#{#filterForm.ctSoft} IS NULL OR :#{#filterForm.ctSoft} != 'Y' OR (CASE WHEN pfc.ctSoft >= 0.48 THEN 'Y' ELSE 'N' END) = 'Y') "
-			+
-			"  AND (:#{#filterForm.ctFlat} IS NULL OR :#{#filterForm.ctFlat} != 'Y' OR (CASE WHEN pfc.ctFlat >= 1.74 THEN 'Y' ELSE 'N' END) = 'Y') "
-			+
-			"  AND (:#{#filterForm.ctPretty} IS NULL OR :#{#filterForm.ctPretty} != 'Y' OR (CASE WHEN pfc.ctPretty >= 34.96 THEN 'Y' ELSE 'N' END) = 'Y') "
-			+
-			"  AND (:#{#filterForm.ctCute} IS NULL OR :#{#filterForm.ctCute} != 'Y' OR (CASE WHEN pfc.ctCute >= 1.14 THEN 'Y' ELSE 'N' END) = 'Y') "
-			+
-			"  AND (:#{#filterForm.ctNeat} IS NULL OR :#{#filterForm.ctNeat} != 'Y' OR (CASE WHEN pfc.ctNeat >= 2.19 THEN 'Y' ELSE 'N' END) = 'Y') "
-			+
-			"  AND (:#{#filterForm.ctModern} IS NULL OR :#{#filterForm.ctModern} != 'Y' OR (CASE WHEN pfc.ctModern >= 0.52 THEN 'Y' ELSE 'N' END) = 'Y') "
-			+
-			"  AND (:#{#filterForm.ctHip} IS NULL OR :#{#filterForm.ctHip} != 'Y' OR (CASE WHEN pfc.ctHip >= 17.95 THEN 'Y' ELSE 'N' END) = 'Y') "
-			+ // 휘뚜루마뚜루
-			"  AND (:#{#filterForm.ctWide} IS NULL OR :#{#filterForm.ctWide} != 'Y' OR (CASE WHEN pfc.ctWide >= 20.7 THEN 'Y' ELSE 'N' END) = 'Y') "
-			+ // 발볼이 넓은
-			"  AND (:#{#filterForm.ctNarrow} IS NULL OR :#{#filterForm.ctNarrow} != 'Y' OR (CASE WHEN pfc.ctNarrow >= 15.47 THEN 'Y' ELSE 'N' END) = 'Y') "
-			+ // 발볼이 좁은
-			"  AND (:#{#filterForm.ctStandard} IS NULL OR :#{#filterForm.ctStandard} != 'Y' OR (CASE WHEN pfc.ctStandard >= 23.66 THEN 'Y' ELSE 'N' END) = 'Y') "
-			+ // 발볼이 보통
-			"  AND (:#{#filterForm.ctCost} IS NULL OR :#{#filterForm.ctCost} != 'Y' OR (CASE WHEN pfc.ctCost >= 10.1 THEN 'Y' ELSE 'N' END) = 'Y') "
-			+ // 가격이 착한
-			"  AND (:#{#filterForm.ctStrong} IS NULL OR :#{#filterForm.ctStrong} != 'Y' OR (CASE WHEN pfc.ctStrong >= 3.12 THEN 'Y' ELSE 'N' END) = 'Y') ORDER BY p.sentiment DESC") // 튼튼한
-	List<ProductAndCategoryDTO> findProductAndCategoryByFilter(@Param("filterForm") FilterForm filterForm);
-
 	// 상품명, 브랜드, 그룹, 타입 검색
 	@Query("SELECT NEW com.smhrd.mueossa.dto.ProductAndCategoryDTO(" +
 			"   p.pId, " +
